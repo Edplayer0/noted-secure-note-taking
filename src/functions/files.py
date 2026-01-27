6d5eacefb81f16e32dd16be88c56cee8ada1ba66
Edplayer0 <ayusomartinezedgar@gmail.com>
2026-01-26 20:06:39 -0500

    incresase the documentation on files.py throught comments and docstrings

--- src/functions/files.py
+++ src/functions/files.py
@@ -1,3 +1,5 @@
+"""Files manager module"""
+
 import sys
 import sqlite3
 
@@ -8,14 +10,17 @@ from src.mediator.mediator import Mediator
 
 def app_files(app_mediator: Mediator) -> None:
     """Adds a event called 'files' to the mediator
-    which returns a dict with the paths"""
+    which returns a dict with the paths to each file"""
 
-    # ARCHIVOS DE LA APLICACION
+    # Folder in which the files are stored
     files_folder = Path.cwd() / "files"
 
+    # If the folder doesn't exist it's created
+    # with the necessary files
     if not files_folder.exists():
         files_folder.mkdir()
 
+        # create the database with sqlite3
         with sqlite3.connect(files_folder / "notas.db") as conn:
             cursor = conn.cursor()
             cursor.execute(
@@ -25,18 +30,23 @@ def app_files(app_mediator: Mediator) -> None:
             )
         conn.commit()
 
+        # create the password file
         with open(files_folder / "password.json", "w", encoding="utf-8") as password:
             password.write("[]")
 
     files = {}
 
+    # add the paths to the dictionary
     files["PASSWORD_FILE"] = files_folder / "password.json"
     files["DATABASE"] = files_folder / "notas.db"
     files["BACKUP_DATABASE"] = files_folder / "notas-backup.db"
 
+    # if it's an executable, use the bitmap
+    # added on pyinstaller installation
     if hasattr(sys, "frozen"):
         files["ICON"] = Path(sys._MEIPASS) / "bitmap.ico"
     else:
         files["ICON"] = Path.cwd() / "assets" / "bitmap.ico"
 
+    # add a handler which returns the dict
     app_mediator.add_handler("files", lambda: files)
