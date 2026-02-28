76128d40cb9b0b73ebf7882bb4dd11be7ee2ef1d
Edplayer0 <ayusomartinezedgar@gmail.com>
2026-02-28 10:04:23 -0500

    feature: implement an "export to txt" option on the menu, allowing to export all notes already decrypted to a single txt file

--- src/ui/menu/functs.py
+++ src/ui/menu/functs.py
@@ -3,11 +3,15 @@
 import shutil
 from tkinter import messagebox
 
+from pathlib import Path
+
 from src.ui.menu.register import Register
+from src.mediator.mediator import Mediator
+from src.models.note_models import NoteData
 
 
 @Register.registry(" MAKE BACKUP")
-def backup_notes(app_mediator):
+def backup_notes(app_mediator: Mediator) -> None:
     """Create a backup of the notes database."""
     database = app_mediator.call_event("files")["DATABASE"]
     backup_path = app_mediator.call_event("files")["BACKUP_DATABASE"]
@@ -16,7 +20,7 @@ def backup_notes(app_mediator):
 
 
 @Register.registry(" RESTORE BACKUP")
-def restore_notes(app_mediator):
+def restore_notes(app_mediator: Mediator) -> None:
     """Restore the notes database from a backup."""
 
     confirm = messagebox.askyesno(
@@ -34,3 +38,46 @@ This will overwrite your current notes.",
     shutil.copy(backup_path, database)
 
     app_mediator.call_event("load_frames")
+
+
+@Register.registry(" EXPORT TO TXT")
+def export_to_txt(app_mediator: Mediator) -> None:
+    """Export all the notes to a txt file"""
+
+    confirm = messagebox.askyesno(
+        "Confirm",
+        "Do you want to export all notes? Every note will be decrypted and accesible in a txt file.",
+    )
+
+    if not confirm:
+        return
+
+    file_content = ""
+
+    notes: list[NoteData] = app_mediator.call_event("load_notes")[::-1]
+
+    line = f"{"-" * 30}\n\n"
+
+    for note in notes:
+
+        if file_content:
+            separator = line
+        else:
+            separator = "\n"
+
+        title = note.title.upper()
+        date = note.date
+        content = app_mediator.call_event("load_note_content", note.id)
+
+        string = f"{separator}{title}  ({date}) \n\n{content}\n\n"
+
+        file_content += string
+
+    file_path = Path("notes.txt")
+
+    with open(file_path, "w", encoding="UTF-8") as file:
+        file.write(file_content)
+
+    messagebox.showinfo(
+        "Export", f"Notes successfully exported to: '{str(file_path.absolute())}'"
+    )
